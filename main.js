275cf50bf038490e76e3c6666eba0bb6e2055052
Dan The Man <18626012+abstractsins@users.noreply.github.com>
2022-04-17 02:12:41 -0400

    added api functionality to codewars section

--- main.js
+++ main.js
@@ -33,6 +33,22 @@ const dropdown = id => {
 }
 /* END 1 -- SECTION REVEAL FUNCTION */
 
+/* 2 -- CODEWARS API */
+const url = 'https://www.codewars.com/api/v1/users/abstractsins';
+const codewarsData = fetch(url).then(res=>res.json()).then(obj=>{
+    $('#honor').append(obj.honor);
+    $('#rank').append(obj.ranks.overall.name);
+    $('#score').append(obj.ranks.overall.score);
+    $('#completed').append(obj.codeChallenges.totalCompleted);
+    $('.five').append(Object.keys(obj.ranks.languages).join(', '));
+    return obj;
+}).catch(err=>{console.error('Something went wrong getting the information!');console.error(err)})
+
+
+
+/* END 2 -- CODEWARS API */
+
+
 
 /* THIS SPACE INTENTIONALLY LEFT BLANK */
 //
